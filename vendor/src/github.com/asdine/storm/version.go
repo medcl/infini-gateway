19182938a96685bd71ceccdfbb7bd59d3888e4cb
medcl <m@medcl.net>
2019-07-16 16:08:42 +0800

    remove vendor

--- vendor/src/github.com/asdine/storm/version.go
+++
@@ -1,4 +0,0 @@
-package storm
-
-// Version of Storm
-const Version = "0.6.1"
--- vendor/src/github.com/mafredri/cdp/protocol/doc.go
+++
@@ -1,5 +0,0 @@
-// Package protocol contains subpackages representing all domains for
-// the Chrome Debugging Protocol.
-//
-// This package also contains some types shared between the subpackages.
-package protocol
